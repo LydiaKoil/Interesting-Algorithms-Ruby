5a09101f5372f761867c3bf67aedc1efded2b834
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-05-07 11:45:07 +0100

    added some more tests

--- test_percentage_filled.rb
+++ test_percentage_filled.rb
@@ -13,4 +13,12 @@ class PercentageFilledTest < Minitest::Test
   def test_rounded_answer
     assert_equal '31%', percentage_filled(['######', '#ooo #', '#oo  #', '#    #', '#    #', '######'])
   end
+
+  def test_full_filled
+    assert_equal '100%', percentage_filled(['######', '#oooo#', '#oooo#', '#oooo#', '#oooo#', '######'])
+  end
+
+  def test_empty
+    assert_equal '0%', percentage_filled(['######', '#     #', '#     #', '#     #', '#     #', '######'])
+  end
 end
